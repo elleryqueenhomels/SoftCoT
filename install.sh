f1a0741018eef9ff1e7034671c3c6bd163d31976
Wengao Ye <elleryqueenhomels@hotmail.com>
2025-09-08 12:06:10 +0100

    Add install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+pip install fastNLP==0.7.0 torch==2.4.1 transformers==4.44.2 datasets pandas peft
+cd extract_judge_answer/latex2sympy
+pip install -e .
+pip install math-verify
+pip install word2number
+cd ../..
